44d553e3271586bfbe52888da4740a0790a70296
Pranav Sastry <pranava.sri@gmail.com>
2021-05-15 01:43:44 +0530

    Final version

--- data_extractor.py
+++ data_extractor.py
@@ -15,33 +15,34 @@ def modify(fname):
     for d in data:
         new_dict = {}
         for k in d:
-            if(k in {"created_on","last_verified_on"}):
-                try:
-                    new_dict[k] = pd.to_datetime(d[k])
-                except:
-                    new_dict[k] = pd.to_datetime("")
-            elif(k in {"resource_type","resource type","resource type_2"}):
-                if("resource_type" not in new_dict):
-                    new_dict["resource_type"] = ""
-                if(d[k]):
-                    new_dict["resource_type"]+="{}  ".format(d[k])
-            elif(k=="Sales/Renta/Charity"):
-                new_dict["Sales or Rental or Charity"]=d[k]
-            elif(k in {"Details","description","Other comments","comment","Sales/Renta/Charity"}):
-                if("comments" not in new_dict):
-                    new_dict["comments"] = ""
-                if(d[k]):
-                    new_dict["comments"]+="{}  ".format(d[k])
-            elif(k=="phone_1"):
-                new_dict["phone"] = d[k]
-            elif(k=="phone_2"):
-                new_dict["phone2"] = d[k]
-            elif(k=="quantity_available"):
-                new_dict[k] = True if d[k] not in ["Unavailable","Undetermined","no beds currently avilable","NIL"] else False
-            elif(k=="verification_status"):
-                new_dict[k] = True if (d[k].lower().startswith("verified") or "and verified" in d[k].lower() or "but verified" in d[k].lower()) else False
-            elif(k in {"hospital_available_normal_beds","hospital_available_ventilator_beds","hospital_available_icu_beds","district","city","title","email","state","pin_code","pincode","address","price","category","source_link","hospital_available_oxygen_beds","latitude","longitude"}):
-                new_dict[k] = d[k]
+            if(d[k]):
+                if(k in {"created_on","last_verified_on"}):
+                    try:
+                        new_dict[k] = pd.to_datetime(d[k])
+                    except:
+                        pass
+                elif(k in {"resource_type","resource type","resource type_2"}):
+                    if("resource_type" not in new_dict):
+                        new_dict["resource_type"] = ""
+                    new_dict["resource_type"]+="{}".format(d[k])
+                elif(k in {"Details","description","Other comments","comment","Sales/Renta/Charity"}):
+                    if("comments" not in new_dict):
+                        new_dict["comments"] = ""
+                    if(k == "Sales/Renta/Charity"):
+                        d[k] = "Sales or Rental or Charity? - " + d[k]
+                    new_dict["comments"]+="{}\n".format(d[k])
+                elif(k=="phone_1"):
+                    new_dict["phone"] = d[k]
+                elif(k=="phone_2"):
+                    new_dict["phone2"] = d[k]
+                elif(k=="quantity_available"):
+                    new_dict[k] = True if d[k] not in ["Unavailable","Undetermined","no beds currently avilable","NIL"] else False
+                elif(k=="verification_status"):
+                    new_dict[k] = True if (d[k].lower().startswith("verified") or "and verified" in d[k].lower() or "but verified" in d[k].lower()) else False
+                elif(k in {"pin_code","pincode"}):
+                    new_dict["pincode"] = d[k]
+                elif(k in {"hospital_available_normal_beds","hospital_available_ventilator_beds","hospital_available_icu_beds","district","city","title","email","state","pin_code","pincode","address","price","category","source_link","hospital_available_oxygen_beds","latitude","longitude"}):
+                    new_dict[k] = d[k]
         new_data.append(new_dict)
         del new_dict
     new_db = {"data":new_data}
