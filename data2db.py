7f70e91dfb305e6c3508558749b6df23f45c8020
Pranav Sastry <pranava.sri@gmail.com>
2021-06-06 15:37:09 +0530

    Fixed issues with contact foreign_key, reverted back to bulk_update and bulk_insert

--- data2db.py
+++ data2db.py
@@ -9,47 +9,65 @@ import datetime
 import pandas
 from db import Supply, Demand, Contact, get_session
 
-def get_existing_entry(table, data_dict, data_source):
-    with get_session() as session:
-        if(data_source=="coronasafe"):
-            existing_entry = session.query(table).filter_by(external_uuid=data_dict['external_uuid']).first()
-        elif(data_source in {"nlp_supply","nlp_demand"}):
-            existing_entry = session.query(table).filter_by(row_num=data_dict['row_num']).first()
-        return existing_entry
-
 def data2db(table, new_data, data_source):
-    contacts_objs = []
-    table_objs = []
-    for d in new_data:
-        contact_fields = ["source","tg_user_id","tg_user_handle"]
-        if(any(item in contact_fields for item in list(d.keys()))):
-            contact_dict = {}
-            for entry in contact_fields:
-                if(entry in list(d.keys())):
-                    if(entry=="tg_user_handle"):
-                        contact_dict["user_handle"] = d.get(entry,"")
-                    else:
-                        contact_dict[entry] = d.get(entry)
-                    d.pop(entry)
-            if(contact_dict):
-                contact = Contact(**contact_dict)
-                contacts_objs.append(contact)
-                d.update(contact=contact)
-                existing_entry = get_existing_entry(table, d, data_source)
-                if(existing_entry):
-                    try:
-                        if(d.get('last_verified_on', datetime.datetime(1950, 1, 1)) > last_verified):
-                            table_objs.append(table(**d))
-                    except:
-                        pass
-                else:
-                    table_objs.append(table(**d))
+    if(data_source=="coronasafe"):
+        has_contact_obj = True
+    elif(data_source in {"nlp_supply","nlp_demand"}):
+        has_contact_obj = True
+    with get_session() as session:
+        table_objs_updates = []
+        table_objs_inserts = []
+        contact_objs_updates = []
+        contact_objs_inserts = []
+        for (i,d) in enumerate(new_data):
+            contact_fields = ["source","tg_user_id","tg_user_handle"]
+            if(any(item in contact_fields for item in list(d.keys()))):
+                contact_dict = {}
+                for entry in contact_fields:
+                    if(entry in list(d.keys())):
+                        if(entry=="tg_user_handle"):
+                            contact_dict["user_handle"] = d.get(entry,"")
+                        else:
+                            contact_dict[entry] = d.get(entry)
+                        if(entry!="source"):
+                            d.pop(entry)
+            if(data_source=="coronasafe"):
+                existing_entry = session.query(table).filter_by(external_uuid=d['external_uuid']).first()
+            elif(data_source in {"nlp_supply","nlp_demand"}):
+                existing_entry = session.query(table).filter_by(row_num=d['row_num']).first()
+            if(existing_entry):
+                last_verified = existing_entry.last_verified_on or datetime.datetime(1960, 1, 1)
+                try:
+                    if(d.get('last_verified_on', datetime.datetime(1950, 1, 1)) > last_verified):
+                        if(has_contact_obj):
+                            contact_dict['id'] = existing_entry.contact_id
+                            contact_objs_updates.append(contact_dict)
+                        d['id'] = existing_entry.id
+                        table_objs_updates.append(d)
+                except:
+                    pass
+            else:
+                if(has_contact_obj):
+                    contact_objs_inserts.append(contact_dict)
+                table_objs_inserts.append(d)
 
     with get_session() as session:
         try:
-            session.bulk_save_objects(contacts_objs)
+            if(has_contact_obj):
+                session.bulk_update_mappings(Contact,contact_objs_updates)
+            session.bulk_update_mappings(table,table_objs_updates)
             session.commit()
-            session.bulk_save_objects(table_objs)
+        except Exception as e:
+            print("Error while saving: " + str(e))
+
+
+    with get_session() as session:
+        try:
+            if(has_contact_obj):
+                session.bulk_insert_mappings(Contact,contact_objs_inserts,return_defaults=True)
+                for (i,d) in enumerate(table_objs_inserts):
+                    d['contact_id'] = contact_objs_inserts[i]['id']
+            session.bulk_insert_mappings(table,table_objs_inserts)
             session.commit()
         except Exception as e:
             print("Error while saving: " + str(e))
